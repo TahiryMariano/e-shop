a41d7600085a77e7b38c96775d39c7b73875bfd4
TahiryMariano <tahirymariano.htm@gmail.com>
2023-10-09 16:36:54 +0300

    fix(homepage):change import of featured component

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,8 +1,8 @@
-import styles from "./Home.style.scss";
+// import styles from "./Home.style.scss";
 import Slider from "../../components/display/slider/Slider";
-import FeaturedProduct from "./section/FeaturedProduct/FeaturedProduct";
 import Categories from "./section/Categories/Categories";
 import Contact from "../../components/common/contact/Contact";
+import FeaturedProduct from "./section/FeaturedProduct/FeaturedProduct";
 
 const Home = () => {
   return (
