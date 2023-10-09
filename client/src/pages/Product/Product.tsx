24176edf6d5e9ffacaf3e0764cfda054ace78281
TahiryMariano <tahirymariano.htm@gmail.com>
2023-10-09 16:33:14 +0300

    feat(product-page):fetch product by id

--- client/src/pages/Product/Product.tsx
+++ client/src/pages/Product/Product.tsx
@@ -1,73 +1,118 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BalanceIcon from "@mui/icons-material/Balance";
 import styles from "./Product.module.scss";
-
-const images = [
-  "https://picsum.photos/id/146/640/320",
-  "https://picsum.photos/id/175/640/320",
-];
+import { useParams } from "react-router-dom";
+import ProductService from "../../services/Product.service";
 
 const Product = () => {
-  const [selectedImg, setSelectedImg] = useState(0);
+  const [selectedImg, setSelectedImg] = useState("img");
   const [quantity, setQuantity] = useState(1);
+  const [data, setData] = useState() as any;
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const id = parseInt(useParams().id!, 10);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const res = await ProductService.getProductById(id);
+        setData(res.data);
+        console.log(data);
+      } catch (error) {
+        setError(true);
+        console.log("error fetching product data", error);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className={styles.product}>
-      <div className={styles.left}>
-        <div className={styles.images}>
-          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
-          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
-        </div>
-        <div className={styles.mainImg}>
-          <img src={images[selectedImg]} alt="" />
-        </div>
-      </div>
-      <div className={styles.right}>
-        <h1>Title</h1>
-        <span className={styles.price}>$133</span>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam earum ut
-          laboriosam ad sed perferendis nihil dicta illo sunt incidunt.
-        </p>
-        <div className={styles.quantity}>
-          <button
-            onClick={(e) => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}
-          >
-            -
-          </button>
-          <span>{quantity}</span>
-          <button onClick={(e) => setQuantity((prev) => prev + 1)}>+</button>
-        </div>
-        <button className={styles.add}>
-          <AddShoppingCartIcon />
-          Add to cart
-        </button>
-        <div className={styles.links}>
-          <div className={styles.item}>
-            <FavoriteBorderIcon />
-            Add to wishlist
+      {loading ? (
+        "loading..."
+      ) : (
+        <>
+          <div className={styles.left}>
+            <div className={styles.images}>
+              <img
+                src={
+                  import.meta.env.VITE_REACT_APP_UPLOAD_URL +
+                  data?.attributes.img.data.attributes.url
+                }
+                alt="side image 1"
+                onClick={(e) => setSelectedImg("img")}
+              />
+              <img
+                src={
+                  import.meta.env.VITE_REACT_APP_UPLOAD_URL +
+                  data?.attributes.img2.data.attributes.url
+                }
+                alt="side image 2"
+                onClick={(e) => setSelectedImg("img2")}
+              />
+            </div>
+            <div className={styles.mainImg}>
+              <img
+                src={
+                  import.meta.env.VITE_REACT_APP_UPLOAD_URL +
+                  data?.attributes[selectedImg].data.attributes.url
+                }
+                alt="main image"
+              />
+            </div>
           </div>
-          <div className={styles.item}>
-            <BalanceIcon />
-            Add to compare
+          <div className={styles.right}>
+            <h1>{data.attributes.title}</h1>
+            <span className={styles.price}>${data.attributes.price}</span>
+            <p>{data.attributes.description}</p>
+            <div className={styles.quantity}>
+              <button
+                onClick={(e) =>
+                  setQuantity((prev) => (prev === 1 ? 1 : prev - 1))
+                }
+              >
+                -
+              </button>
+              <span>{quantity}</span>
+              <button onClick={(e) => setQuantity((prev) => prev + 1)}>
+                +
+              </button>
+            </div>
+            <button className={styles.add}>
+              <AddShoppingCartIcon />
+              Add to cart
+            </button>
+            <div className={styles.links}>
+              <div className={styles.item}>
+                <FavoriteBorderIcon />
+                Add to wishlist
+              </div>
+              <div className={styles.item}>
+                <BalanceIcon />
+                Add to compare
+              </div>
+            </div>
+            <div className={styles.info}>
+              <span>Vendor: Polo</span>
+              <span>Product Type: T-shirt</span>
+              <span>Tag: T-shirt,women, top</span>
+            </div>
+            <hr />
+            <div className={styles.details}>
+              <span>DESCRIPTION</span>
+              <hr />
+              <span>ADDITIONAL INFORMATION</span>
+              <hr />
+              <span>FAQ</span>
+            </div>
           </div>
-        </div>
-        <div className={styles.info}>
-          <span>Vendor: Polo</span>
-          <span>Product Type: T-shirt</span>
-          <span>Tag: T-shirt,women, top</span>
-        </div>
-        <hr />
-        <div className={styles.details}>
-          <span>DESCRIPTION</span>
-          <hr />
-          <span>ADDITIONAL INFORMATION</span>
-          <hr />
-          <span>FAQ</span>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };
