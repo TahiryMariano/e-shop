e42a6b16d2fac6e64edfe0fc0ac0763707f1882f
TahiryMariano <tahirymariano.htm@gmail.com>
2023-10-10 16:48:30 +0300

    feat(store):persist store

--- client/process.d.ts
+++ client/process.d.ts
@@ -1,6 +1,6 @@
-declare namespace NodeJS{
-    export interface ProcessEnv{
-        VITE_REACT_APP_API_TOKEN:string;
-        VITE_REACT_APP_API_URL:string
-    }
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    VITE_REACT_APP_API_TOKEN: string;
+    VITE_REACT_APP_API_URL: string;
+  }
 }
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,14 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./features/CartSlice"
+import { persistStore, persistReducer } from "redux-persist";
+import cartReducer from "./features/CartSlice";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, cartReducer);
 
 export const store = configureStore({
-    reducer:{
-        cart:cartReducer
-    }
-})
+  reducer: {
+    cart: persistedReducer,
+  },
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const persistor = persistStore(store);
