ce1c4c830daad99f6581a70b03a2151e74d55236
TahiryMariano <tahirymariano.htm@gmail.com>
2023-10-12 13:05:17 +0300

    feat(cart):handle payment

--- client/src/components/display/cart/Cart.tsx
+++ client/src/components/display/cart/Cart.tsx
@@ -1,9 +1,10 @@
-import React from "react";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import styles from "./Cart.module.scss";
 import { AppDispatch, useAppSelector } from "../../../store/store";
 import { removeItem, resetCart } from "../../../store/features/CartSlice";
 import { useDispatch } from "react-redux";
+import { loadStripe } from "@stripe/stripe-js";
+import { api } from "../../../api/api";
 
 const Cart = () => {
   const data = [
@@ -38,6 +39,24 @@ const Cart = () => {
     return total.toFixed(2);
   };
 
+  const stripePromise = loadStripe(
+    "pk_test_51NzgNzDrFJqiAxvnUbYo5fICE1bczs5qAZy7wX2q1DY7HBtrr72xFW4hdtrd23rW1wYs6yta1zAwhurYld1ovT0g00dC4fqpdf"
+  );
+
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+      const res = await api.post("/orders", { products });
+      console.log(res);
+
+      await stripe?.redirectToCheckout({
+        sessionId: res?.data.stripeSession.id,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className={styles.cart}>
       <h1>Products in your cart</h1>
@@ -64,7 +83,9 @@ const Cart = () => {
         <span>TOTAL</span>
         <span>${totalPrice()}</span>
       </div>
-      <button className={styles.checkout}>PROCEED TO CHECKOUT</button>
+      <button className={styles.checkout} onClick={handlePayment}>
+        PROCEED TO CHECKOUT
+      </button>
       <span className={styles.reset} onClick={() => dispatch(resetCart())}>
         Reset cart
       </span>
